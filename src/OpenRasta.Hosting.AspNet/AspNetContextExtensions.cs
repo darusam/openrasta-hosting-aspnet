9a05a1a97747e2804e3d4e5e63e7f856c4704934
darusam@cdmsmith.com <darusam@cdmsmith.com>
2014-10-01 00:12:12 -0400

    bug fixes for AspNetContextExtensions (these will soon become unsupported)

--- src/OpenRasta.Hosting.AspNet/AspNetContextExtensions.cs
+++ src/OpenRasta.Hosting.AspNet/AspNetContextExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,11 +9,23 @@ namespace OpenRasta.Hosting.AspNet
     {
         public static object GetSessionVariable(this AspNetCommunicationContext context, int index)
         {
+            if (context == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (context.NativeContext == null)
+            {
+                throw new MemberAccessException();
+            }
+            if (context.NativeContext.Session == null)
+            {
+                throw new NullReferenceException();
+            }
             return context.NativeContext.Session[index];
         }
         public static T GetSessionVariable<T>(this AspNetCommunicationContext context, int index)
         {
-            object toConvert = context.NativeContext.Session[index];
+            object toConvert = context.GetSessionVariable(index);
             if (toConvert == null)
             {
                 return default(T);
@@ -24,6 +37,10 @@ namespace OpenRasta.Hosting.AspNet
                     T toReturn = (T)toConvert;
                     return toReturn;
                 }
+                catch (InvalidCastException ice)
+                {
+                    throw ice;
+                }
                 catch (Exception e)
                 {
                     return default(T);
@@ -33,11 +50,33 @@ namespace OpenRasta.Hosting.AspNet
         }
         public static object GetSessionVariable(this AspNetCommunicationContext context, string name)
         {
-            return context.NativeContext.Session[name];
+            if (context == null || string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException();
+            }
+            if (context.NativeContext == null)
+            {
+                throw new MemberAccessException();
+            }
+            if (context.NativeContext.Session == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            object toReturn = null;
+            for (int i = 0; i < context.NativeContext.Session.Count; i++)
+            {
+                if (context.NativeContext.Session.Keys[i] == name)
+                {
+                    toReturn = context.NativeContext.Session[i];
+                    break;
+                }
+            }
+            return toReturn;
         }
         public static T GetSessionVariable<T>(this AspNetCommunicationContext context, string name)
         {
-            object toConvert = context.NativeContext.Session[name];
+            object toConvert = context.GetSessionVariable(name);
             if(toConvert == null)
             {
                 return default(T);
@@ -49,6 +88,10 @@ namespace OpenRasta.Hosting.AspNet
                     T toReturn = (T)toConvert;
                     return toReturn;
                 }
+                catch(InvalidCastException ice)
+                {
+                    throw ice;
+                }
                 catch(Exception e)
                 {
                     return default(T);
@@ -59,11 +102,52 @@ namespace OpenRasta.Hosting.AspNet
 
         public static void SetSessionVariable(this AspNetCommunicationContext context, int index, object value)
         {
+            if (context == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (context.NativeContext == null)
+            {
+                throw new MemberAccessException();
+            }
+            if (context.NativeContext.Session == null)
+            {
+                throw new NullReferenceException();
+            }
             context.NativeContext.Session[index] = value;
         }
         public static void SetSessionVariable(this AspNetCommunicationContext context, string name, object value)
         {
+            if (context == null || string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException();
+            }
+            if (context.NativeContext == null)
+            {
+                throw new MemberAccessException();
+            }
+            if (context.NativeContext.Session == null)
+            {
+                throw new NullReferenceException();
+            }
             context.NativeContext.Session[name] = value;
+        }
+
+        public static void AddSessionVariable(this AspNetCommunicationContext context, string name, object value)
+        {
+            if (context == null || string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException();
+            }
+            if (context.NativeContext == null)
+            {
+                throw new MemberAccessException();
+            }
+            if (context.NativeContext.Session == null)
+            {
+                throw new NullReferenceException();
+            }
+            context.NativeContext.Session.Add(name, value);
         }
     }
 }
